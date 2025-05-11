e7f850a9f177bf000d81a5d04139da1c4ad1adc0
Emainfix <fev.artist@gmail.com>
2025-05-11 13:04:49 -0300

    Pagina Rivarola

---
+++ app.js
@@ -0,0 +1,51 @@
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+
+let lengthItems = items.length - 1;
+let active = 0;
+
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(()=> {next.click()}, 5000);
+
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(()=> {next.click()}, 5000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+})
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        targetElement.scrollIntoView({
+        behavior: 'smooth'
+        });
+    });
+    });
